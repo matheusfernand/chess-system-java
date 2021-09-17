a3251fb282f42301b22b964a9b705d03f9242849
matheusfernand <theusf_47@hotmail.com>
2021-09-17 00:28:22 -0300

    move - Castling

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,32 +9,44 @@ import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
-public class Program {
-	public static void main(String[] args) {
 
+public class Program {
+
+	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-
-		while (true) {
+		
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-
+				
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-
+				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
+				
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
+				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
+					chessMatch.replacePromotedPiece(type);
 				}
 			}
 			catch (ChessException e) {
@@ -46,5 +58,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,24 +17,23 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
-
+	
 	public int getMoveCount() {
 		return moveCount;
 	}
-
-	public void increaseMoveCount() {
+	
+	protected void increaseMoveCount() {
 		moveCount++;
 	}
 
-	public void decreaseMoveCount() {
-		moveCount++;
+	protected void decreaseMoveCount() {
+		moveCount--;
 	}
-
 
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
-
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
